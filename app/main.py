ad42e2e8a2426377587f1fe26d83462afcc804ba
przemekz <przemyslaw.zarnecki@wroclaw.merito.pl>
2025-09-25 15:38:15 +0200

    Wersja z prostym demem dla klienta. Przygotowana pod serwis render

--- app/main.py
+++ app/main.py
@@ -1,12 +1,62 @@
 # app/main.py
 # demo, z obsługą html
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from pathlib import Path
+#from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 from app.api.link4 import router as link4_router
+from pathlib import Path
 
+app = FastAPI(title="Insurance API")
+
+# Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # można wpisać konkretną domenę jak ["http://localhost:5500"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Mountowanie folderu frontend jako statyczny
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+
+# Główna strona
+@app.get("/", response_class=HTMLResponse)
+async def serve_index():
+    html_path = Path("frontend/index.html")
+    return HTMLResponse(content=html_path.read_text(encoding="utf-8"))
+
+# Strona wyników kalkulacji
+@app.post("/kalkulacja", response_class=HTMLResponse)
+async def handle_kalkulacja(
+    first_name: str = Form(...),
+    last_name: str = Form(...),
+    license_plate: str = Form(...),
+    car_make: str = Form(...),
+    car_model: str = Form(...),
+    car_year: int = Form(...),
+    start_date: str = Form(...),
+    insurance_formula: int = Form(...)
+):
+    html_path = Path("frontend/kalkulacja.html")
+    return HTMLResponse(content=html_path.read_text(encoding="utf-8"))
+
+# Nowa strona kalkulatora - formularza (formularz_do pracy.html)
+@app.get("/formularz", response_class=HTMLResponse)
+async def serve_formularz():
+    html_path = Path("frontend/formularz.html")
+    return HTMLResponse(content=html_path.read_text(), status_code=200)
+
+# Routing API
+app.include_router(link4_router, prefix="/api/link4", tags=["Link4"])
+
+
+
+
+'''
 app = FastAPI(title="Insurance API")
 
 # ✅ Tu dodaj CORS (dla działania formularza HTML lokalnie)
@@ -28,6 +78,7 @@ def serve_form():
 
 # API
 app.include_router(link4_router, prefix="/api/link4", tags=["Link4"])
+'''
 '''
 #wersja pierwotna sprzed dema
 
